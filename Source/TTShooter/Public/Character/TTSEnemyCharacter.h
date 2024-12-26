0329c85c4fb552d70630e019df05dc134b89ab9b
Natwm <natw.mezza@hotmail.fr>
2024-12-26 18:23:51 +0100

    Nmezza - [Add] Base enemy logics

---
+++ Source/TTShooter/Public/Character/TTSEnemyCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/TTSBaseCharacter.h"
+#include "TTSEnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TTSHOOTER_API ATTSEnemyCharacter : public ATTSBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
